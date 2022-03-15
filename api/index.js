6f4fe6a7c947713d75b0771191d5c456006051b1
l4p1n <l4p1n@l4p1n.ch>
2022-03-15 15:42:34 +0100

    Refactor application server to close the HTTP server and pool gracefully before exiting

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
+const http = require('http');
+const {closePool} = require('./database/pool');
+
 function handle_termination(){
-  console.log("SIGTERM or SIGINT caught. Exiting");
-  process.exit();
+  console.log("SIGTERM or SIGINT caught. Exiting gracefully");
+
+  closePool(() => { console.log('Database pool closed'); });
+  server.close(() => { console.log('API server closed'); });
 }
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +15,6 @@ const app = require('./application');
 process.on("SIGTERM", handle_termination);
 process.on("SIGINT", handle_termination)
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}.`);
-});
+const server = http.createServer(app).listen(PORT, () => {
+  console.log(`API server listening on ${PORT}`);
+})
