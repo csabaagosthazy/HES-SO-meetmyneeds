22251a241595d648487888a74a9fc110d3a99316
l4p1n <l4p1n@l4p1n.ch>
2022-03-15 14:49:03 +0100

    Remove unnecessary routes in the server

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../database/queries");
 
 router.get("/", async (req, res) => {
   const message = {}
@@ -9,11 +8,5 @@ router.get("/", async (req, res) => {
       .setHeader("Content-Type", "application/json")
       .send(JSON.stringify(message));
 });
-
-router.get("/users", db.getUsers);
-router.get("/users/:id", db.getUserById);
-router.post("/users", db.createUser);
-router.put("/users/:id", db.updateUser);
-router.delete("/users/:id", db.deleteUser);
 
 module.exports = router;
