2615bbdebca3d6d3d17d3ad0dbd46e82e032594d
l4p1n <l4p1n@l4p1n.ch>
2022-03-15 15:29:20 +0100

    Turn database pool into a singleton

---
+++ api/database/pool.js
@@ -0,0 +1,28 @@
+const {Pool} = require("pg");
+const host = process.env.POSTGRES_HOST ?? 'database';
+
+// Turn the database pool into a singleton
+let DATABASE_POOL = undefined;
+
+module.exports = {
+    getPool: () => {
+        if(DATABASE_POOL === undefined) {
+            DATABASE_POOL = new Pool({
+                user: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                database: "meetmyneeds",
+                host
+            });
+        }
+
+        return DATABASE_POOL;
+    },
+
+    closePool: () => {
+        if(DATABASE_POOL === undefined){
+            return;
+        }
+
+        DATABASE_POOL.end();
+    }
+}
