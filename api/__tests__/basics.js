3370e156b54d15fb2ff427b6c580260d3452a3ad
l4p1n <l4p1n@l4p1n.ch>
2022-03-15 14:48:33 +0100

    Revise server basics tests

--- api/__tests__/basics.js
+++ api/__tests__/basics.js
@@ -1,17 +1,18 @@
 const request = require('supertest');
-const { Response: TestResponse } = require('superagent');
+const { Response } = require('superagent');
 const app = require('../application');
 
 describe("Test root path", () => {
     it("should return 200", async () => {
-        /** @type TestResponse */
+        /** @type Response */
         const response = await request(app).get("/api");
         expect(response.status).toEqual(200);
     });
 
     it('should set the content-type to JSON', async () => {
-        /** @type TestResponse */
+        /** @type Response */
         const response = await request(app).get("/api")
         expect(response.headers['content-type']).toMatch(/^application\/json/)
+        expect(response.body).toEqual({})
     })
 })
