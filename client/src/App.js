c4fca92f52cee778a5b3b603bb65200dee773f49
csabaagosthazy <agosthazy.csaba@gmail.com>
2022-03-17 13:52:40 +0100

    apptest

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Questionnaire from "./components/pages/Questionnaire";
+import Counter from "./components/counter/Counter";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Header</header>
+      <Counter>My counter</Counter>
       <Questionnaire id={1} lang={"en"} />
     </div>
   );
