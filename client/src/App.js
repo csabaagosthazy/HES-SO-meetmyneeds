84b4e504970d682cc405332b0057a9a50fc36d45
csabaagosthazy <agosthazy.csaba@gmail.com>
2022-03-18 13:14:12 +0100

    counter added to app (again)

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Counter>My counter</Counter>
         <Questionnaire id={1} lang={"en"} />
       </header>
     </div>
