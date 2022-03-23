1f95665aa74953b68cdfa39d52ddd68711c2dae5
l4p1n <l4p1n@l4p1n.ch>
2022-03-23 10:53:14 +0100

    Add test to test the presence of color_set in the categories API

--- api/database/repository/category.js
+++ api/database/repository/category.js
@@ -7,7 +7,7 @@ module.exports = {
         let results = await pool.query(
             `SELECT DISTINCT q.category_id,
                              qc.label,
-                             qc.question_category_color_set
+                             qc.question_category_color_set AS color_set
              FROM question_category qc
                       INNER JOIN questions q on qc.category_id = q.category_id
              WHERE q.sub_category_id IS NULL
