f42c4dde4265984059316591712718b4d2aa03e7
l4p1n <l4p1n@l4p1n.ch>
2022-03-23 11:00:43 +0100

    Add JSDoc to the categories repository on method get_main_categories

--- api/database/repository/category.js
+++ api/database/repository/category.js
@@ -1,6 +1,12 @@
 const {getPool} = require('../pool');
 
 module.exports = {
+    /**
+     * Queries for main question categories, that is categories not associated to
+     * the sub_category_id column in the question table.
+     *
+     * @returns {Promise<{category_id: string, label: string, color_set: string}>}
+     */
     get_main_categories: async () => {
         const pool = getPool();
 
