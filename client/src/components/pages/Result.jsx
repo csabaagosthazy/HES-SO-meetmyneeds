1d49c95bbebe9c5c21c4d5e447e8bc7ffd273cca
baptisteostertag <baptiste.ostertag@netplus.ch>
2022-03-25 11:12:06 +0100

    Result page show only categories concerned by one question at least

--- client/src/components/pages/Result.jsx
+++ client/src/components/pages/Result.jsx
@@ -1,80 +1,92 @@
-import react from 'react';
-
-/*
-    weight: essential = 1, important = 2, a bit important = 3
-*/
 const answers = [{
-    question_id:1, 
-    question:"Actually I need info about diagnostics",
+    question_id: 1,
+    question: "Actually I need info about diagnostics",
     answer_id: 3,
-    label:"a bit important",
+    label: "A bit important",
     technicalKey: "less_important"
 
-}, 
+},
 
-{
-    question_id: 2, 
-    question:"Why?",
-    answer_id : 1, 
-    label : "essential", 
-    technicalKey: "essential"
-}, 
+    {
+        question_id: 2,
+        question: "Why?",
+        answer_id: 1,
+        label: "Essential",
+        technicalKey: "essential"
+    },
 
-{
-    question_id: 3, 
-    question:"Who?",
-    answer_id : 1, 
-    label : "essential", 
-    technicalKey: "essential"
-}
+    {
+        question_id: 3,
+        question: "Who?",
+        answer_id: 1,
+        label: "Essential",
+        technicalKey: "essential"
+    },
+    {
+        question_id: 4,
+        question: "This is a test?",
+        answer_id: 2,
+        label: "Important",
+        technicalKey: "important"
+    }
 ]
 
-const Result = (props) =>{
+const Result = (props) => {
 
     const json_toStr = JSON.stringify(answers);
     const obj = JSON.parse(json_toStr);
 
     //sets to store questions grouped by importance of need 
-    let essential = new Set(); 
-    let important = new Set(); 
+    let essential = new Set();
+    let important = new Set();
     let less_important = new Set();
     let already_filled = new Set();
 
     //grouping by importance of need 
     obj.forEach(element => {
-        if(element.technicalKey === "essential"){
+        if (element.technicalKey === "essential") {
             essential.add([element.question_id, element.question]);
-        }else if(element.technicalKey === "important"){
+        } else if (element.technicalKey === "important") {
             important.add([element.question_id, element.question])
-        }else if(element.technicalKey === "less_important"){
+        } else if (element.technicalKey === "less_important") {
             less_important.add([element.question_id, element.question])
-        }else if(element.technicalKey === "already_filled"){
+        } else if (element.technicalKey === "already_filled") {
             already_filled.add([element.question_id, element.question])
         }
     });
 
-    console.log("Essentials", essential);
-    console.log("Important", important);
-    console.log("Less important", less_important);
-    console.log("Already filled", already_filled);
-
     //create empty dict or set ???
-    /*const labels = [...new Set(obj.map(item => item.label))];
-    console.log("Groupped", labels)*/
+    var labels = new Map();
+    obj.map(item => labels.set(item.technicalKey, item.label));
 
-    return(
+    return (
         <div>
-           {
-                answers.map((answer,index) => {
-                    return(
-                        <div key={index}>
-                            {answer.label}
-                        </div>
-                    );
-                })
-            }
+            <p>
+                {
+                    essential.size !== 0 ? labels.get("essential") : ""
+
+                }
+            </p>
+            <p>
+                {
+                    important.size !== 0 ? labels.get("important") : ""
+
+                }
+            </p>
+            <p>
+                {
+                    less_important.size !== 0 ? labels.get("less_important") : ""
+
+                }
+            </p>
+            <p>
+                {
+                    already_filled.size !== 0 ? labels.get("already_filled") : ""
+
+                }
+            </p>
         </div>
     );
-}
+};
 
 export default Result; 
