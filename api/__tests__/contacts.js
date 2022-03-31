0eefd756732d4fdb62deea3aece44b603f346d20
baptisteostertag <baptiste.ostertag@netplus.ch>
2022-03-31 08:53:57 +0200

    contacts Endpoint API

---
+++ api/__tests__/contacts.js
@@ -0,0 +1,62 @@
+const request = require('supertest');
+const app = require('../application');
+const {getPool} = require("../database/pool");
+
+describe('resources API', () => {
+    it('rejects missing ID parameter', async () => {
+        const response = await request(app).get('/api/contacts');
+        expect(response.status).toEqual(400);
+    })
+
+    it('accepts correct request', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.status).toEqual(200);
+    })
+
+    it('returns a JSON payload', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.headers['content-type']).toMatch(/^application\/json/);
+
+        try{
+            JSON.parse(response.text);
+        } catch (e) {
+            fail("Response payload is not JSON.parse-able");
+        }
+    })
+
+    it('responds with a service_id key', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.body[0].hasOwnProperty('service_id')).toBe(true);
+    })
+
+    it('responds with a name key', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.body[0].hasOwnProperty('name')).toBe(true);
+    })
+
+    it('responds with a description key', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.body[0].hasOwnProperty('description')).toBe(true);
+    })
+
+    it('responds with a address key', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.body[0].hasOwnProperty('address')).toBe(true);
+    })
+
+    it('responds with a question_id key', async () => {
+        const response = await request(app).get('/api/contacts?question_id=1');
+        expect(response.body[0].hasOwnProperty('question_id')).toBe(true);
+    })
+
+    it('returns right contacts according to the question', async () => {
+        const contacts_for_question_id_1 = ["1", "2"];
+        const response = await request(app).get('/api/contacts?question_id=1');
+        const returned_contacts_ids = response.body.map(r => r.service_id);
+
+        expect(returned_contacts_ids.length).toEqual(contacts_for_question_id_1.length)
+        for(returned_contacts_id of returned_contacts_ids){
+            expect(contacts_for_question_id_1.indexOf(returned_contacts_id)).not.toBe(-1);
+        }
+    })
+})
---
+++ api/database/repository/contact.js
@@ -0,0 +1,28 @@
+const {getPool} = require('../pool');
+
+module.exports = {
+    /**
+     * Queries for contacts related to one question
+     *
+     * @returns {Promise<{service_id: int, question_id: int, name: string, description: string, name: string, address: string}>}
+     */
+    get_question_contacts: async (question_id) => {
+        const pool = getPool();
+
+        let results = await pool.query(
+            `SELECT sq.service_id, sq.question_id,
+                    s.name, s.description,
+                    o.name, o.address
+            FROM service_question sq
+                      INNER JOIN services s
+            on sq.service_id = s.service_id
+                      INNER JOIN organizations o
+            on s.organization_id = o.organization_id
+             WHERE sq.question_id = $1
+             ORDER BY sq.question_id`,
+            [question_id]
+        );
+
+        return results.rows;
+    }
+}
