fc39837693369482f4723f904233a7823202913c
Irina Tregub <irinatregub@gmail.com>
2022-04-03 23:28:50 +0300

    Added displaying organizations

--- client/src/components/pages/Result.jsx
+++ client/src/components/pages/Result.jsx
@@ -1,77 +1,80 @@
-import React from "react";
+import React, {useState} from "react";
 import * as commonmark from "commonmark";
-import { COLORS } from "../../global/colors";
-import { getContacts, getResources } from "../../services/api/service";
+import {COLORS} from "../../global/colors";
 import CustomButton from "../button/CustomButton";
+import Contacts from "./Contacts";
 
 const Result = (props) => {
-  const reader = new commonmark.Parser();
-  const writer = new commonmark.HtmlRenderer();
+    const [contacts, showContacts] = useState(false);
+    const reader = new commonmark.Parser();
+    const writer = new commonmark.HtmlRenderer();
 
-  const obj = JSON.parse(sessionStorage.getItem("answers"));
-  const arr = obj.results;
-  console.log(obj);
+    const obj = JSON.parse(sessionStorage.getItem("answers"));
+    const arr = obj.results;
+    console.log(obj);
 
-  //sets to store questions grouped by importance of need
-  let essential = new Set();
-  let important = new Set();
-  let less_important = new Set();
-  let already_filled = new Set();
+    //sets to store questions grouped by importance of need
+    let essential = new Set();
+    let important = new Set();
+    let less_important = new Set();
+    let already_filled = new Set();
 
-  //grouping by importance of need
-  arr.forEach((element) => {
-    switch (element.technicalKey) {
-      case "essential":
-        essential.add([element.question_id, element.question]);
-        break;
-      case "important":
-        important.add([element.question_id, element.question]);
-        break;
-      case "less_important":
-        less_important.add([element.question_id, element.question]);
-        break;
-      case "already_filled":
-        already_filled.add([element.question_id, element.question]);
-        break;
-    }
-  });
-
-  //create a set of labels
-  var labels = new Map();
-  arr.map((item) => labels.set(item.technicalKey, item.label));
-
-  //function that displays questions in set under the certain label
-  const displayQuestions = (questionSet, labelSet, label_tag) => {
-    let questions_render = [...questionSet].map((item) => {
-      const question_to_parse = reader.parse(item[1]);
-      const question_to_display = writer.render(question_to_parse);
-      <h1 dangerouslySetInnerHTML={{ __html: question_to_display }} />;
-      return(
-        <div>
-          <p key={item[0]} dangerouslySetInnerHTML={{ __html: question_to_display }} />
-          <CustomButton variant={"s"} bgColor={"lightgrey"}>Qui contacter?</CustomButton>
-          <CustomButton variant={"s"} bgColor={"yellow"}>Ressources</CustomButton>
-        </div>);
+    //grouping by importance of need
+    arr.forEach((element) => {
+        switch (element.technicalKey) {
+            case "essential":
+                essential.add([element.question_id, element.question]);
+                break;
+            case "important":
+                important.add([element.question_id, element.question]);
+                break;
+            case "less_important":
+                less_important.add([element.question_id, element.question]);
+                break;
+            case "already_filled":
+                already_filled.add([element.question_id, element.question]);
+                break;
+        }
     });
-    questions_render.unshift(
-      <h3 style={{ backgroundColor: COLORS[obj.color][label_tag] }} key={label_tag}>
-        {labelSet.get(label_tag)}
-      </h3>
-    ); //prepending label
-    return questions_render;
-  };
 
-  return (
-    <div>
-      {essential.size !== 0 ? displayQuestions(essential, labels, "essential") : ""}
+    //create a set of labels
+    var labels = new Map();
+    arr.map((item) => labels.set(item.technicalKey, item.label));
 
-      {important.size !== 0 ? displayQuestions(important, labels, "important") : ""}
+    //function that displays questions in set under the certain label
+    const displayQuestions = (questionSet, labelSet, label_tag) => {
+        let questions_render = [...questionSet].map((item) => {
+            const question_to_parse = reader.parse(item[1]);
+            const question_to_display = writer.render(question_to_parse);
+            <h1 dangerouslySetInnerHTML={{__html: question_to_display}}/>;
+            return (
+                <div key={item[0]}>
+                    <p dangerouslySetInnerHTML={{__html: question_to_display}}/>
+                    <CustomButton variant={"s"} bgColor={"lightgrey"} onClick={() => showContacts(true)}>Qui
+                        contacter?</CustomButton>
+                    <Contacts showContacts={contacts} onHide={() => showContacts(false)} question_id={item[0]}/>
+                    <CustomButton variant={"s"} bgColor={"yellow"}>Ressources</CustomButton>
+                </div>);
+        });
+        questions_render.unshift(
+            <h3 style={{backgroundColor: COLORS[obj.color][label_tag]}} key={label_tag}>
+                {labelSet.get(label_tag)}
+            </h3>
+        ); //prepending label
+        return questions_render;
+    };
 
-      {less_important.size !== 0 ? displayQuestions(less_important, labels, "less_important") : ""}
+    return (
+        <div>
+            {essential.size !== 0 ? displayQuestions(essential, labels, "essential") : ""}
 
-      {already_filled.size !== 0 ? displayQuestions(already_filled, labels, "already_filled") : ""}
-    </div>
-  );
+            {important.size !== 0 ? displayQuestions(important, labels, "important") : ""}
+
+            {less_important.size !== 0 ? displayQuestions(less_important, labels, "less_important") : ""}
+
+            {already_filled.size !== 0 ? displayQuestions(already_filled, labels, "already_filled") : ""}
+        </div>
+    );
 };
 
 export default Result;
