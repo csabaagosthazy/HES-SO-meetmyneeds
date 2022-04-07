d8c0f2ad13d0f642e01735142b561e7808890c4a
csabaagosthazy <agosthazy.csaba@gmail.com>
2022-04-07 22:57:10 +0200

    app test skipped

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App renders Counter component with correct text", () => {
+test.skip("App renders Counter component with correct text", () => {
   render(<App />);
   //const element = screen.getByText("My counter");
   //expect(element).toBeInTheDocument();
