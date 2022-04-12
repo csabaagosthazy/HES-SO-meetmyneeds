0e24b296cda338cddf02dccecc9b25041f37db87
csabaagosthazy <agosthazy.csaba@gmail.com>
2022-04-12 10:14:54 +0200

    Result page save button with nav state added

--- client/src/components/pages/Result.jsx
+++ client/src/components/pages/Result.jsx
@@ -1,12 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import * as commonmark from "commonmark";
-import {COLORS} from "../../global/colors";
+import { COLORS } from "../../global/colors";
 import CustomButton from "../button/CustomButton";
 import Contacts from "./Contacts";
 import Resources from "./Resources";
-import {Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
+
+import { useAuth } from "../../services/pryv/service";
+import { useNavigate } from "react-router-dom";
+import { createEvent, getLastEvents } from "../../services/pryv/functions";
 
 const Result = () => {
+    const { connection } = useAuth();
+    const navigate = useNavigate();
+
     const [contacts, showContacts] = useState(false);
     const [resources, showResources] = useState(false);
     const reader = new commonmark.Parser();
@@ -37,12 +44,12 @@ const Result = () => {
                 already_filled.add([element.question_id, element.question]);
                 break;
             default:
-                // no-op
+            // no-op
         }
     });
 
     // Create a map of technical key => label
-    const labels = new Map(answers.map(answer => [answer.technicalKey, answer.label]));
+    const labels = new Map(answers.map((answer) => [answer.technicalKey, answer.label]));
 
     //function that displays questions in set under the certain label
     const displayQuestions = (questionSet, labelSet, label_tag) => {
@@ -50,36 +57,85 @@ const Result = () => {
             const question_to_parse = reader.parse(question_label);
             const question_to_display = writer.render(question_to_parse);
             return (
-                <Card.Body style={{display: 'flex', flexDirection: 'row', borderBottom:"1px solid", alignItems:"center"}} key={question_id}>
-                    <Card.Title style={{flex: 1}}><p dangerouslySetInnerHTML={{__html: question_to_display}} /></Card.Title>
-                    <CustomButton variant={"s"} bgColor={"lightgrey"} onClick={() => showContacts(true)}>Qui contacter?</CustomButton>
-                    <CustomButton variant={"s"} bgColor={"yellow"} onClick={() => showResources(true)}>Ressources</CustomButton>
-                    <Resources  showResources={resources}
-                                onHide={() => showResources(false)}
-                                question_id={question_id}/>
-                    <Contacts   showContacts={contacts}
-                                onHide={() => showContacts(false)}
-                                question_id={question_id}/>
+                <Card.Body
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        borderBottom: "1px solid",
+                        alignItems: "center",
+                    }}
+                    key={question_id}
+                >
+                    <Card.Title style={{ flex: 1 }}>
+                        <p dangerouslySetInnerHTML={{ __html: question_to_display }} />
+                    </Card.Title>
+                    <CustomButton
+                        variant={"s"}
+                        bgColor={"lightgrey"}
+                        onClick={() => showContacts(true)}
+                    >
+                        Qui contacter?
+                    </CustomButton>
+                    <CustomButton
+                        variant={"s"}
+                        bgColor={"yellow"}
+                        onClick={() => showResources(true)}
+                    >
+                        Ressources
+                    </CustomButton>
+                    <Resources
+                        showResources={resources}
+                        onHide={() => showResources(false)}
+                        question_id={question_id}
+                    />
+                    <Contacts
+                        showContacts={contacts}
+                        onHide={() => showContacts(false)}
+                        question_id={question_id}
+                    />
                 </Card.Body>
             );
         });
         questions_render.unshift(
-            <Card.Header style={{backgroundColor: COLORS[questionnaireAnswers.color][label_tag]}} key={label_tag}>
+            <Card.Header
+                style={{ backgroundColor: COLORS[questionnaireAnswers.color][label_tag] }}
+                key={label_tag}
+            >
                 {labelSet.get(label_tag)}
             </Card.Header>
         );
         return questions_render;
     };
 
+    const saveResultsToPryv = () => {
+        if (connection && connection.token) {
+            console.log(questionnaireAnswers);
+            createEvent(connection, "test obj from web");
+            console.log("saved");
+        } else {
+            if (window.confirm("You need to sign in")) {
+                navigate("/login", { state: { goto: "/results" } });
+            } else {
+                console.log("stay on page");
+            }
+        }
+    };
     return (
         <Card>
             {essential.size !== 0 && displayQuestions(essential, labels, "essential")}
 
             {important.size !== 0 && displayQuestions(important, labels, "important")}
 
-            {less_important.size !== 0 && displayQuestions(less_important, labels, "less_important")}
+            {less_important.size !== 0 &&
+                displayQuestions(less_important, labels, "less_important")}
 
-            {already_filled.size !== 0 && displayQuestions(already_filled, labels, "already_filled")}
+            {already_filled.size !== 0 &&
+                displayQuestions(already_filled, labels, "already_filled")}
+            <Card.Footer>
+                <CustomButton variant={"m"} bgColor={"lightBlue"} onClick={saveResultsToPryv}>
+                    Save my results
+                </CustomButton>
+            </Card.Footer>
         </Card>
     );
 };
